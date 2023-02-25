b5f7310d61e3fd5a263fe561330da08b62761908
farildev <memmedovfaril@gmail.com>
2023-02-25 20:44:49 +0400

    UPDATE

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,23 @@
 const links = document.querySelectorAll('.category__items');
 const imgBox = document.querySelectorAll('.img__box');
-links.forEach(function(link){
-    console.log(link);
-})
+
+for(let i = 0; i<links.length; i++){
+    links[i].addEventListener('click', function(e){
+        for(let k=0; k<links.length; k++){
+            links[k].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        const dataFilter = this.getAttribute('data-filter');
+        for(let j = 0; j<imgBox.length; j++){
+            imgBox[j].classList.add('hide');
+            imgBox[j].classList.remove('active');
+            if(imgBox[j].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+                imgBox[j].classList.remove('hide');
+                imgBox[j].classList.add('active');
+            }
+        }
+            
+        e.preventDefault();
+    });
+}
